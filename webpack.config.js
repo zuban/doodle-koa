fd2ead42e56f2f4d70c10c2f0d3a90c9e1bbce4d
Sergey Zubov <serzbv@gmail.com>
2016-08-29 07:31:04 +0300

    test commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: './build',
+        filename: 'bundle.js',
+    },
+    target: 'node', // in order to ignore built-in modules like path, fs, etc.
+    externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                plugins: ['transform-async-to-generator', 'syntax-async-functions'],
+                presets: ['es2015-node5'],
+            }
+        }]
+    }
+}
